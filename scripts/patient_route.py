6d89be4adf8817b7d97fe8b5eeb1fa70af94afe8
Seongbum Seo <sbum.seo@gmail.com>
2020-11-06 21:57:38 +0900

    Update patient routes

--- scripts/patient_route.py
+++ scripts/patient_route.py
@@ -4,7 +4,11 @@ import numpy as np
 import pyproj
 from fiona.crs import from_epsg
 
-voronoi = [gpd.read_file('data/kr_village_voronoi_intersection.geojson'), gpd.read_file('data/kr_city_county_voronoi_intersection.geojson')]
+voronoi = [
+  gpd.read_file('data/kr_village_voronoi_intersection.geojson'),
+  gpd.read_file('data/kr_city_county_voronoi_intersection.geojson'),
+  gpd.read_file('data/kr_province_voronoi_intersection.geojson')
+]
 
 route = gpd.read_file('data/hidden/patient_route_1106.csv')
 route = route.replace('', np.nan).dropna(subset=['Latitude', 'Longitude'])
@@ -17,6 +21,7 @@ for index, row in route_frame.iterrows():
     cell_data[i][index] = list(voronoi[i][voronoi[i].contains(row.geometry)]['cellid'])[0]
 route_frame['VillageCellID'] = pd.Series(list(cell_data[0].values()), index=list(cell_data[0].keys()), dtype=np.dtype('int32'))
 route_frame['CityCellID'] = pd.Series(list(cell_data[1].values()), index=list(cell_data[1].keys()), dtype=np.dtype('int32'))
+route_frame['ProvinceCellID'] = pd.Series(list(cell_data[2].values()), index=list(cell_data[2].keys()), dtype=np.dtype('int32'))
 #route_frame.assign(emdlid=series)
 print(route_frame)
 route_frame.drop(['geometry', 'PlaceName', 'Memo'], axis=1).to_csv('data/hidden/patient_route_cellid_output.csv')
