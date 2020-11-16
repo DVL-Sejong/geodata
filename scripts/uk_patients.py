9ecc2af60d4d4e4f6c7a4964f5e9e0c62578f3d8
Seongbum Seo <sbum.seo@gmail.com>
2020-11-16 21:45:57 +0900

    Update UK patients data

---
+++ scripts/uk_patients.py
@@ -0,0 +1,20 @@
+import csv
+import json
+
+voronoi_file = open('data/uk_ltla_voronoi_intersection.geojson')
+voronoi = json.load(voronoi_file)['features']
+
+cases_read_file = open('data/hidden/UK Coronavirus Cases 1112.csv')
+cases_reader = csv.DictReader(cases_read_file)
+cases_reader.fieldnames.append('LTLACellID')
+
+cases_write_file = open('data/hidden/uk_patients.csv', mode='w')
+cases_writer = csv.DictWriter(cases_write_file, fieldnames=cases_reader.fieldnames)
+cases_writer.writeheader()
+
+for row in cases_reader:
+  for vor in voronoi:
+    if row['Area code'] == vor['properties']['postcode']:
+      row['LTLACellID'] = vor['properties']['cell_id']
+      break
+  cases_writer.writerow(row)
