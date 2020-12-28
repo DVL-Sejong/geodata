90b21aef4cdec1049d9429f74d59f09dcf0cff05
Seongbum Seo <sbum.seo@gmail.com>
2020-12-28 14:14:38 +0900

    Update

---
+++ scripts/regions_uk.py
@@ -0,0 +1,88 @@
+import csv
+import json
+import requests
+
+region_list = []
+type_list = []
+regions = {}
+
+geodata = {
+  'type': 'FeatureCollection',
+  'features': []
+}
+
+with open('./data/uk_ltla_list.csv') as csv_file:
+  next(csv_file)
+  reader = csv.reader(csv_file)
+
+  for index, region_data in enumerate(reader):
+    region_postcode = region_data[0]
+    region_name = region_data[1]
+    region_fid = region_data[3]
+    region_type = 'ltla'
+    region_country = 'UK'
+
+    response = requests.get(f'https://findthatpostcode.uk/areas/{region_postcode}.json')
+
+    if response.status_code >= 400:
+      raise RuntimeError(f'Request failed: {response.text}')
+    if response.status_code == 200:
+      response_data = response.json()
+      name = response_data['data']['attributes']['name']
+      location = response_data['included'][0]['attributes']['location']
+
+      feature = {
+        'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [float(location['lon']), float(location['lat'])]
+        },
+        'properties': {
+          'fid': region_fid,
+          'cell_id': index,
+          'name': response_data['data']['attributes']['name'],
+          'type': 'ltla',
+          'postcode': region_postcode
+        }
+      }
+
+    if region_type not in type_list:
+      type_list.append(region_type)
+      regions[region_type] = []
+    if region_name not in region_list:
+      region_list.append(region_name)
+      regions[region_type].append({
+        'name': region_name,
+        'type': region_type,
+        'postcode': region_postcode
+      })
+
+for region_type in type_list:
+  geodata = {
+    'type': 'FeatureCollection',
+    'features': []
+  }
+  for index, region in enumerate(regions[region_type]):
+    osm = nominatim.query(f"{region['name']}, England")
+
+    osm = osm.toJSON()[0]
+    feature = {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [float(osm['lon']), float(osm['lat'])]
+      },
+      'properties': {
+        'osmid': osm['osm_id'],
+        'cell_id': index,
+        'name': region['name'],
+        'type': region['type'],
+        'postcode': region['postcode']
+      }
+    }
+    geodata['features'].append(feature)
+
+  with open(f'./data/uk_{region_type}_osm.geojson', 'w', encoding='utf-8') as json_file:
+    dump = json.dumps(geodata, ensure_ascii=False)
+    json_file.write(dump)
+    print(f'[osm-{region_type}] output:', len(geodata['features']))
--- scripts/uk_patients.py
+++ scripts/uk_patients.py
@@ -1,20 +1,60 @@
 import csv
 import json
+import requests
+from datetime import date
+
+file_date = date.today().strftime("%m%d")
+
+regions_file = open('data/uk_ltla_list.csv')
+regions_reader = csv.DictReader(regions_file)
 
 voronoi_file = open('data/uk_ltla_voronoi_intersection.geojson')
 voronoi = json.load(voronoi_file)['features']
 
-cases_read_file = open('data/hidden/UK Coronavirus Cases 1112.csv')
-cases_reader = csv.DictReader(cases_read_file)
-cases_reader.fieldnames.append('LTLACellID')
+cases_data = []
+for region in regions_reader:
+  region_name = region['LAD18NM']
+  region_code = region['LAD18CD']
+  region_cell_id = -1
 
-cases_write_file = open('data/hidden/uk_patients.csv', mode='w')
-cases_writer = csv.DictWriter(cases_write_file, fieldnames=cases_reader.fieldnames)
-cases_writer.writeheader()
-
-for row in cases_reader:
   for vor in voronoi:
-    if row['Area code'] == vor['properties']['postcode']:
-      row['LTLACellID'] = vor['properties']['cell_id']
-      break
+    if region_code == vor['properties']['postcode']:
+      region_cell_id = vor['properties']['cell_id']
+  if region_cell_id == -1:
+    print(f'Warning: cell not found - {region_name}({region_code})')
+
+  endpoint = (
+    'https://api.coronavirus.data.gov.uk/v1/data?'
+    f'filters=areaType=ltla;areaName={region_name}&'
+    'structure={"date":"date","newCases":"newCasesBySpecimenDate","cumCases":"cumCasesBySpecimenDate"}'
+  )
+
+  response = requests.get(endpoint, timeout=10)
+
+  if response.status_code >= 400:
+    raise RuntimeError(f'Request failed: {response.text}')
+  if response.status_code == 200:
+    response_data = response.json()['data']
+    '''try:
+      response_data = response.json()['data']
+    except:
+      print(f'Warning: {response.json()}')
+      continue'''
+
+    for row in response_data:
+      cases_data.append({
+        'Specimen date': row['date'],
+        'New cases': row['newCases'],
+        'Area name': region_name,
+        'Area code': region_code,
+        'Area type': 'ltla',
+        'Cell ID': region_cell_id
+      })
+
+file_name = f'data/hidden/uk_patients_{file_date}.csv'
+cases_file = open(file_name, mode='w')
+cases_writer = csv.DictWriter(cases_file, fieldnames=cases_data[0].keys())
+cases_writer.writeheader()
+for row in cases_data:
   cases_writer.writerow(row)
+print(file_name)
